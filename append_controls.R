4077c6af2871440662947c42fcc0a566dc069fd4
g-tunski <gracetunski@gmail.com>
2022-04-13 20:17:36 -0600

    Adding append controls file

--- append_controls.R
+++ append_controls.R
@@ -1,23 +1,34 @@
 ### Code to add controls to list b/c if in main code, rows will be added multiple times ###
 
-add_euro = data.frame ('Pre-EuroAmerican settlement horizon', '0', 'NA', '0')
-write.table(add_euro, file = "chroncontrol_types-edited.csv", sep = ",",
-            append = TRUE, quote = FALSE,
-            col.names = FALSE, row.names = FALSE)
+# add_euro = data.frame ('Pre-EuroAmerican settlement horizon', '0', 'NA', '0')
+# write.table(add_euro, file = "chroncontrol_types-edited.csv", sep = ",",
+#             append = TRUE, quote = FALSE,
+#             col.names = FALSE, row.names = FALSE)
 
-add_euro2 = data.frame ('European settlement horizon', '0'', 'NA', '0')
-write.table(add_euro, file = "chroncontrol_types-edited.csv", sep = ",",
-            append = TRUE, quote = FALSE,
-            col.names = FALSE, row.names = FALSE)
+# add_euro2 = data.frame ('European settlement horizon', '0', 'NA', '0')
+# write.table(add_euro2, file = "chroncontrol_types-edited.csv", sep = ",",
+#             append = TRUE, quote = FALSE,
+#             col.names = FALSE, row.names = FALSE)
 
-picea_decline = data.frame ('Picea decline', '0', 'NA', '0')
-write.table(picea_decline, file = "chroncontrol_types-edited.csv", sep = ",",
-            append = TRUE, quote = FALSE,
-            col.names = FALSE, row.names = FALSE)
+# picea_decline = data.frame ('Picea decline', '0', 'NA', '0')
+# write.table(picea_decline, file = "chroncontrol_types-edited.csv", sep = ",",
+#             append = TRUE, quote = FALSE,
+#             col.names = FALSE, row.names = FALSE)
 
-picea_decline2 = data.frame ('IDW-2d Picea decline', '0', 'NA', '0')
-write.table(picea_decline2, file = "chroncontrol_types-edited.csv", sep = ",",
-            append = TRUE, quote = FALSE,
-            col.names = FALSE, row.names = FALSE)
+# picea_decline2 = data.frame ('IDW-2d Picea decline', '0', 'NA', '0')
+# write.table(picea_decline2, file = "chroncontrol_types-edited.csv", sep = ",",
+#             append = TRUE, quote = FALSE,
+#             col.names = FALSE, row.names = FALSE)
+# 
+# fire  = data.frame ('Historical fire', '1', 'NA', '1')
+# write.table(fire, file = "chroncontrol_types-edited.csv", sep = ",",
+#             append = TRUE, quote = FALSE,
+#             col.names = FALSE, row.names = FALSE)
+
+# documents  = data.frame ('Historical documentation', '1', 'NA', '1')
+# write.table(documents, file = "chroncontrol_types-edited.csv", sep = ",",
+#             append = TRUE, quote = FALSE,
+#             col.names = FALSE, row.names = FALSE)
+
 
 
